ef6133d51f0a129139eca2e67862c54c02a3a2a8
kukushechkin <kukushechkin@mac.com>
2025-01-21 00:35:26 +0200

    Keep observing
    Signed-off-by: kukushechkin <kukushechkin@mac.com>

--- tests/compile_and_run_c_test.sh
+++ tests/compile_and_run_c_test.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-# Get the directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Get the directory of this script in a POSIX-compatible way
+DIR=$(dirname "$0")
+cd "$DIR"
+DIR=$(pwd)
 
 # Compile the test
 cc "$DIR/test.c" \
